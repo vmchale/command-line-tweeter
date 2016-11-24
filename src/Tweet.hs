ffaa76967d699a4fcef400dbab6f234c6cbbea5d
Vanessa McHale <tmchale@wisc.edu>
2016-11-24 03:17:46 -0600

    idk

--- src/Tweet.hs
+++ src/Tweet.hs
@@ -39,7 +39,7 @@ exec = do
 signRequest :: Request -> IO Request
 signRequest = signOAuth oAuth credential
     where oAuth = newOAuth { oauthConsumerKey = key , oauthConsumerSecret = secret }
-          credential = newCredential key secret
+          credential  = newCredential key secret
           key         = "739626641450635265-qiX6qWR9uoqhoZZjTQfscrWFSjwiHMC"
           secret      = "n7sUjCinVt3abFD7Qgnxd9MnaMFK6OXYS119eWWzR"
 
@@ -47,37 +47,3 @@ inputToTweet :: IO Tweet
 inputToTweet = do
     content <- fmap (take 140) getContents --aka stdin aka compatible w/ pipes
     return Tweet { status = content, trim_user = True }
-
--- | More flexible signOAuth
-signOAuth' :: MonadIO m
-          => OAuth              -- ^ OAuth Application
-          -> Credential         -- ^ Credential
-          -> (BS.ByteString -> Credential -> Request -> Request) -- ^ signature style
-          -> Request            -- ^ Original Request
-          -> m Request          -- ^ Signed OAuth Request
-signOAuth' oa crd add_auth req = do
-    crd' <- addTimeStamp =<< addNonce crd
-    let tok = injectOAuthToCred oa crd'
-    sign <- genSign oa tok req
-    return $ add_auth prefix (insert "oauth_signature" sign tok) req
-    where
-        prefix = case oauthRealm oa of
-            Nothing -> "OAuth "
-            Just v  -> "OAuth realm=\"" `BS.append` v `BS.append` (BS.pack ['"',','])
-
-addNonce :: MonadIO m => Credential -> m Credential
-addNonce cred = do
-  nonce <- liftIO $ replicateM 10 (randomRIO ('a','z')) -- FIXME very inefficient
-  return $ insert "oauth_nonce" (BS.pack nonce) cred
-
-addTimeStamp :: MonadIO m => Credential -> m Credential
-addTimeStamp cred = do
-  stamp <- (floor . (`diffUTCTime` baseTime)) `liftM` liftIO getCurrentTime
-  return $ insert "oauth_timestamp" (BS.pack $ show (stamp :: Integer)) cred
-
-injectOAuthToCred :: OAuth -> Credential -> Credential
-injectOAuthToCred oa cred =
-    inserts [ ("oauth_signature_method", showSigMtd $ oauthSignatureMethod oa)
-            , ("oauth_consumer_key", oauthConsumerKey oa)
-            , ("oauth_version", "1.0")
-            ] cred
