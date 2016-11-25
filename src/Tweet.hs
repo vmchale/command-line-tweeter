4dd866f77cde82546190ac7d2920fd38c8b313ad
Vanessa McHale <tmchale@wisc.edu>
2016-11-25 13:29:14 -0600

    vaguely should work

--- src/Tweet.hs
+++ src/Tweet.hs
@@ -10,9 +10,8 @@ import GHC.Generics
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS
 import Network.HTTP.Types.Status (statusCode)
-import Web.Authenticate.OAuth hiding (signOAuth')
-import qualified Data.ByteString.Char8 as BS
-import Control.Monad.IO.Class (MonadIO)
+import Web.Authenticate.OAuth
+import qualified Data.ByteString.Lazy.Char8 as BS
 
 data Tweet = Tweet
     { status    :: String
@@ -24,26 +23,34 @@ instance ToJSON Tweet where
 --query twitter to post stdin (aka what's passed in via pipes)
 exec :: IO ()
 exec = do
-    tweet <- inputToTweet
-    let requestObject = toJSON tweet
+    requestObject <- inputToTweet
     manager <- newManager tlsManagerSettings
-    initialRequest <- parseRequest "https://api.twitter.com/1.1/statuses/update.json" --"http://httpbin.org/post" to test
-    signedRequest <- signRequest initialRequest
-    print signedRequest
-    let request = signedRequest { method = "POST", requestBody = RequestBodyLBS $ encode requestObject }
-    --response
+    signedRequest <- parseRequest "https://api.twitter.com/1.1/statuses/update.json" >>= signRequest
+    let request = signedRequest { method = "POST" , requestBody = RequestBodyLBS $ encode requestObject }
+    print request
+    response request manager
+
+response :: Request -> Manager -> IO ()
+response request manager = do
     response <- httpLbs request manager
-    putStrLn $ "The status code was: " ++ (show $ statusCode $ responseStatus response)
+    putStrLn $ "The status code was: " ++ show (statusCode $ responseStatus response)
     print $ responseBody response
-
-signRequest :: Request -> IO Request
+    
+signRequest :: Request -> IO Request --uses IO b/c signing requires a timestamp
 signRequest = signOAuth oAuth credential
-    where oAuth = newOAuth { oauthConsumerKey = key , oauthConsumerSecret = secret }
-          credential  = newCredential key secret
-          key         = "739626641450635265-qiX6qWR9uoqhoZZjTQfscrWFSjwiHMC"
-          secret      = "n7sUjCinVt3abFD7Qgnxd9MnaMFK6OXYS119eWWzR"
 
-inputToTweet :: IO Tweet
+oAuth :: OAuth
+oAuth = newOAuth { oauthConsumerKey = key , oauthConsumerSecret = secret , oauthServerName = url }
+    where secret = "EwozUJbFXzXKjoYWdf9zYrxiSagz1eDbEMWLlfjzinn0RGEQrl"
+          key    = "Zi4fdD888qYBYSD7n0QkRv6Yr"
+          url    = "api.twitter.com"
+
+credential :: Credential
+credential = newCredential token secretToken
+    where token       = "739626641450635265-O9qWyuHbglnmCablfw46D95VMnHp10P"
+          secretToken = "mfvMsChCt6VbMCD8XVOjCK7bGbit2BmBvC1YujL3mRzxF"
+
+inputToTweet :: IO Value
 inputToTweet = do
     content <- fmap (take 140) getContents --aka stdin aka compatible w/ pipes
-    return Tweet { status = content, trim_user = True }
+    return $ toJSON Tweet { status = content, trim_user = True }
