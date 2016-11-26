e3950e60f8c9c033a23fcc24196c30b1bf15f990
Vanessa McHale <tmchale@wisc.edu>
2016-11-25 22:02:24 -0600

    multitweets I'm a disaster no one stop me

--- src/Tweet.hs
+++ src/Tweet.hs
@@ -11,8 +11,9 @@ import Network.HTTP.Client
 import Network.HTTP.Client.TLS
 import Network.HTTP.Types.Status (statusCode)
 import Web.Authenticate.OAuth
-import Data.ByteString.Char8 (pack, unpack)
+import qualified Data.ByteString.Char8 as BS
 import Data.Char (toLower)
+import Data.List.Split (chunksOf)
 
 data Tweet = Tweet
     { status    :: String
@@ -24,7 +25,12 @@ instance ToJSON Tweet where
 --query twitter to post stdin (aka what's passed in via pipes)
 exec :: IO ()
 exec = do
-    requestString <- urlString
+    content <- fmap (map BS.pack) $ fmap (chunksOf 140) getContents --aka stdin aka compatible w/ pipes
+    sequence_ $ fmap tweet content
+
+tweet :: BS.ByteString -> IO ()
+tweet content = do
+    requestString <- urlString content
     manager <- newManager tlsManagerSettings
     initialRequest <- parseRequest ("https://api.twitter.com/1.1/statuses/update.json" ++ requestString)
     request <- signRequest $ initialRequest { method = "POST" }
@@ -41,16 +47,15 @@ signRequest = signOAuth oAuth credential
 
 oAuth :: OAuth
 oAuth = newOAuth { oauthConsumerKey = key , oauthConsumerSecret = secret , oauthServerName = url }
-    where secret = "YOUR_SECRET_KEY"
-          key    = "YOUR_API_KEY"
+    where secret = "CzUrfL4lbhdY7qMyGPDq6rVOuNglkTp7fTKEvRfr2P1iIMd16a"
+          key    = "375wBiEuvaxHMjE7OAiDKSveH"
           url    = "api.twitter.com"
 
 credential :: Credential
 credential = newCredential token secretToken
-    where token       = "TOKEN"
-          secretToken = "TOKEN_SECRET"
+    where token       = "802268569585287168-mL9GTRozVUQRH3g7yLr0TQ2vx3AFuiY"
+          secretToken = "P0a2vMaNvBJaismFjeQSzUccpetB1LzeNql9OsNcFhj92"
 
-urlString :: IO String
-urlString = do
-    content <- fmap (pack . (take 140)) getContents --aka stdin aka compatible w/ pipes
-    return $ "?status=" ++ ((unpack . paramEncode) content) ++ "&trim_user" ++ (map toLower $ show True)
+urlString :: BS.ByteString -> IO String
+urlString content = do
+    return $ "?status=" ++ ((BS.unpack . paramEncode) content) ++ "&trim_user" ++ (map toLower $ show True)
