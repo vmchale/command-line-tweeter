d063cd526ab7906c47f219191f13e19460ad5730
Vanessa McHale <tmchale@wisc.edu>
2016-12-12 00:36:42 -0600

    fixed documentation

--- src/Tweet.hs
+++ src/Tweet.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric     #-}
 
+-- | Various utilities to tweet using the twitter api
 module Tweet
     ( exec
     ) where
@@ -15,6 +16,7 @@ import qualified Data.ByteString.Char8 as BS
 import Data.Char (toLower)
 import Data.List.Split (chunksOf)
 
+-- | Data type for our request
 data Tweet = Tweet
     { status    :: String
     , trim_user :: Bool
@@ -22,12 +24,13 @@ data Tweet = Tweet
 
 instance ToJSON Tweet where
 
---query twitter to post stdin (aka what's passed in via pipes)
+-- | query twitter to post stdin
 exec :: IO ()
 exec = do
     content <- fmap ((take 4) . (map BS.pack) .  (chunksOf 140)) getContents --aka stdin aka compatible w/ pipes
     sequence_ $ fmap tweet content
 
+-- | tweet a byteString
 tweet :: BS.ByteString -> IO ()
 tweet content = do
     requestString <- urlString content
@@ -36,26 +39,32 @@ tweet content = do
     request <- signRequest $ initialRequest { method = "POST" }
     response request manager
 
+-- | print output of a request
 response :: Request -> Manager -> IO ()
 response request manager = do
     response <- httpLbs request manager
     putStrLn $ "The status code was: " ++ show (statusCode $ responseStatus response)
     print $ responseBody response
-    
-signRequest :: Request -> IO Request --uses IO b/c signing requires a timestamp
+
+-- | Sign a request using your OAuth dev token.
+-- Uses the IO monad because signatures require a timestamp
+signRequest :: Request -> IO Request
 signRequest = signOAuth oAuth credential
 
+-- | Create an OAuth token
 oAuth :: OAuth
 oAuth = newOAuth { oauthConsumerKey = key , oauthConsumerSecret = secret , oauthServerName = url }
     where secret = "CzUrfL4lbhdY7qMyGPDq6rVOuNglkTp7fTKEvRfr2P1iIMd16a"
           key    = "375wBiEuvaxHMjE7OAiDKSveH"
           url    = "api.twitter.com"
 
+-- | Create a new credential from a token and secret component of that token
 credential :: Credential
 credential = newCredential token secretToken
     where token       = "802268569585287168-mL9GTRozVUQRH3g7yLr0TQ2vx3AFuiY"
           secretToken = "P0a2vMaNvBJaismFjeQSzUccpetB1LzeNql9OsNcFhj92"
 
+-- | Convert a byteString to the percent-encoded version
 urlString :: BS.ByteString -> IO String
 urlString content = do
     return $ "?status=" ++ ((BS.unpack . paramEncode) content) ++ "&trim_user" ++ (map toLower $ show True)
