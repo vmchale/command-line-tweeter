b4e12e0a3c13e1cdc0964c4136958f0697586971
Vanessa McHale <tmchale@wisc.edu>
2017-04-24 22:54:42 -0500

    nix

--- default.nix
+++ default.nix
@@ -1,18 +1,19 @@
 { mkDerivation, aeson, ansi-wl-pprint, authenticate-oauth, base
-, bytestring, composition, data-default, directory, extra, hspec
-, hspec-megaparsec, http-client, http-client-tls, http-types, lens
-, megaparsec, MissingH, optparse-applicative, split, stdenv, text
+, bytestring, composition, containers, data-default, directory
+, extra, hspec, hspec-megaparsec, http-client, http-client-tls
+, http-types, lens, megaparsec, MissingH, optparse-applicative
+, split, stdenv, text
 }:
 mkDerivation {
   pname = "tweet-hs";
-  version = "0.5.3.4";
+  version = "0.5.3.8";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
     aeson ansi-wl-pprint authenticate-oauth base bytestring composition
-    data-default directory extra http-client http-client-tls http-types
-    lens megaparsec MissingH optparse-applicative split text
+    containers data-default directory extra http-client http-client-tls
+    http-types lens megaparsec MissingH optparse-applicative split text
   ];
   executableHaskellDepends = [ base ];
   testHaskellDepends = [
